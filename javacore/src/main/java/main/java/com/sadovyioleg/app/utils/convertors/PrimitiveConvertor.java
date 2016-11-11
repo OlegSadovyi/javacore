fe7ff45d6120684670694912f4a775358b83f48e
sadovyioleg <oleg.sad@i.ua>
2016-11-11 20:23:38 +0200

    3commit

---
+++ javacore/src/main/java/main/java/com/sadovyioleg/app/utils/convertors/PrimitiveConvertor.java
@@ -0,0 +1,7 @@
+package main.java.com.sadovyioleg.app.utils.convertors;
+
+/**
+ * Created by Oleg on 11.11.16.
+ */
+public class PrimitiveConvertor {
+}
---
+++ javacore/src/main/java/runners/Lesson3Runner.java
@@ -0,0 +1,6 @@
+package main.java.com.sadovyioleg.app.utils.runners;
+
+    public static void main (String args[]) {
+
+
+    }
